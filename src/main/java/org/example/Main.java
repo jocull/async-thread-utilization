592154e2eff70260ac29d750a2ef62e05a138bf0
James O'Cull <james.ocull@gmail.com>
2023-07-17 21:56:26 -0400

    Converted to Spring Boot application

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
-public class Main {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOGGER.info("STARTING THE APPLICATION");
+        SpringApplication.run(Main.class, args);
+        LOGGER.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
